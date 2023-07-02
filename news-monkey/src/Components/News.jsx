d830fda3b272e0c92d446f8da6b9ac7d6b3be805
Varun-Kolanu <112728411+Varun-Kolanu@users.noreply.github.com>
2023-07-02 11:41:15 +0530

    Added Infinite scroll

--- news-monkey/src/Components/News.jsx
+++ news-monkey/src/Components/News.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 import Spinner from './Spinner';
 import PropTypes from 'prop-types'
+import InfiniteScroll from 'react-infinite-scroll-component';
 
 export class News extends Component {
 
@@ -24,7 +25,7 @@ export class News extends Component {
         super(props);
         this.state = {
             articles: [],
-            loading: false,
+            loading: true,
             page: 1,
             totalResults: 0
         }
@@ -57,25 +58,43 @@ export class News extends Component {
         this.updateNews();
     }
 
+    fetchMoreData = async () => {
+        this.setState({ page: this.state.page + 1 });
+        const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=ff1b6a42901a4c5db04989f7f408de53&page=${this.state.page}&pageSize=${this.props.pageSize}`
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        this.setState({
+            articles: this.state.articles.concat(parsedData.articles),
+            totalResults: parsedData.totalResults,
+        });
+    }
     render() {
         return (
-            <div className='container my-3'>
+            <>
                 <h1 className='text-center' >News Monkey - {this.capitalize(this.props.category)} Headlines</h1>
                 {this.state.loading && <Spinner />}
-                <div className="row">
-                    {(!this.state.loading) &&
-                        this.state.articles.map((elmt) => {
-                            return <div key={elmt.url} className="col-md-4">
-                                <NewsItem title={elmt.title ? (elmt.title) : ""} description={elmt.description ? (elmt.description) : ""} imageUrl={elmt.urlToImage ? elmt.urlToImage : "https://c.ndtvimg.com/2023-05/fgthcvg_shubman-gill-bcci_625x300_26_May_23.jpg?im=FeatureCrop,algorithm=dnn,width=1200,height=675"} newsUrl={elmt.url} author={elmt.author} time={elmt.publishedAt} source={elmt.source.name} />
-                            </div>
-                        })
-                    }
-                </div>
-                <div className='container d-flex justify-content-between'>
+                <InfiniteScroll
+                    dataLength={this.state.articles.length}
+                    next={this.fetchMoreData}
+                    hasMore={this.state.articles.length !== this.state.totalResults}
+                    loader={<Spinner />}
+                >
+                    <div className='container my-3'>
+                        <div className="row">
+                            {this.state.articles.map((elmt) => {
+                                    return <div key={elmt.url} className="col-md-4">
+                                        <NewsItem title={elmt.title ? (elmt.title) : ""} description={elmt.description ? (elmt.description) : ""} imageUrl={elmt.urlToImage ? elmt.urlToImage : "https://c.ndtvimg.com/2023-05/fgthcvg_shubman-gill-bcci_625x300_26_May_23.jpg?im=FeatureCrop,algorithm=dnn,width=1200,height=675"} newsUrl={elmt.url} author={elmt.author} time={elmt.publishedAt} source={elmt.source.name} />
+                                    </div>
+                                })
+                            }
+                        </div>
+                    </div>
+                </InfiniteScroll>
+                {/* <div className='container d-flex justify-content-between'>
                     <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
                     <button disabled={this.state.page + 1 > (Math.ceil(this.state.totalResults / this.props.pageSize))} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
-                </div>
-            </div>
+                </div> */}
+            </>
         )
     }
 }
